c233b8270a78c06a8696072bb6cdb18c45a51aea
Thomas Amb <antihackertnt@gmail.com>
2018-06-28 23:47:51 +0200

    Create a prototype datastore module

---
+++ coffeebox/scripts/datastore.js
@@ -0,0 +1,55 @@
+
+
+(function(window){
+    'use strict'
+
+    //IIFE Code goes here ;)
+    //window will be injected as parameter
+    var App = window.App || {}; // {} == new object
+
+    
+
+    //Constructor
+    function DataStore(){
+        this.data = {}; //Property object
+    }
+
+    //
+    // ## Modification functions ## 
+    //
+
+    //Add value to same key will overwrite the old value
+    DataStore.prototype.add = function(key, value) {
+        this.data[key] = value;
+    };
+
+    DataStore.prototype.update = function(key, value) {
+        if (this.data[key]) {
+            console.log("key: " + key + " found! Update to value: " + value)
+            this.data[key] = value;
+        }else{
+            console.log("key: " + key + " not found")
+        }
+        
+    };
+
+    DataStore.prototype.remove = function(key) {
+        delete this.data[key]; //delete removes a key/value pair of an object !!
+    };
+
+    //
+    // ## Return functions ## 
+    //
+    DataStore.prototype.get = function (key) {
+        return this.data[key];
+    };
+
+    DataStore.prototype.getAll = function () {
+        return this.data;
+    };
+
+
+    App.DataStore = DataStore;
+    window.App = App;
+    
+})(window)
