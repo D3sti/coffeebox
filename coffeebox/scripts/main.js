467b6dce5c5a4a16eeb74d0b9a835dd7f573087c
Thomas Amb <antihackertnt@gmail.com>
2018-06-29 17:28:26 +0200

    Add Formhandler Submit on main.js (Test)

--- coffeebox/scripts/main.js
+++ coffeebox/scripts/main.js
@@ -2,19 +2,22 @@
 
     'use strict';
 
+    var FORM_SELECTOR='[data-coffee-order="form"]';//'#datacoffeeform'//'[data-coffee-order="form"]';
+
     var App = window.App;
     var Truck = App.Truck;
     var DataStore = App.DataStore;
-
+    var FormHandler = App.FormHandler;
     var truckId = "FTR-558"
 
     //Dummy testdata
     var foodTruck = new Truck(truckId, new DataStore());
-    foodTruck.createOrder({ emailAddress: "Sam", coffee: "Kaukau"});
-    foodTruck.createOrder({ emailAddress: "Freddy", coffee: "Tee"});
-    foodTruck.createOrder({ emailAddress: "Sepp", coffee: "Kaffee"});
-
+    
     //Export truck object to global namespace ('window') as property
     window.foodTruck = foodTruck;
+    var formHandler = new FormHandler(FORM_SELECTOR);
+
+    formHandler.addSubmitHandler();
+    console.log(formHandler);
 
 })(window)
