240d6f8a180d91ccfd42bc62aa9ca765dc141ff1
Antihacker <Antihacker@DESKTOP-QPATC2L>
2018-08-05 10:56:39 +0200

    Add input Validation for coffee and email

---
+++ coffeebox/scripts/validation.js
@@ -0,0 +1,22 @@
+(function(window){
+
+    'use strict';
+
+    var App = window.App || {}
+
+    var Validation = {
+
+        //RegEx for email validation
+        //The regex obj has a test method (test(email))
+        isCompanyEmail: function(email){
+            return /.+@foodtruckers\.com$/.test(email);
+        }
+
+    };
+
+
+    App.Validation = Validation;
+    window.App = App;
+
+
+})(window)
